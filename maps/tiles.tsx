4a2476d4d038019df223830dcf2c409b5cfa32d4
Staś Małolepszy <stas@piesku.com>
2020-07-01 10:27:27 +0200

    Add Tiled map files

---
+++ maps/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.1" name="tiles" tilewidth="32" tileheight="32" tilecount="25" columns="5">
+ <image source="tiles.png" width="160" height="160"/>
+</tileset>
